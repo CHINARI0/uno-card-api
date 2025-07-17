5f860124c507199996365d1fe31f4d4fde8d11d6
Çınar <cinarbirgili@gmail.com>
2025-07-17 03:18:14 -0400

    Initial commit

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask, request, jsonify
 from ultralytics import YOLO
 import cv2
 import numpy as np
 
 app = Flask(__name__)
-model = YOLO("best.pt")  # Make sure best.pt is in the same folder
+model = YOLO("best.pt")  # your trained model
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -19,3 +20,7 @@ def predict():
     detected_classes = [model.names[int(cls)] for cls in results[0].boxes.cls]
 
     return jsonify({"detections": detected_classes})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 if not set
+    app.run(host="0.0.0.0", port=port)
